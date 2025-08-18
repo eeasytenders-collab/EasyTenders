3518f7c163d8fd3506dbaa284d7f99e2cb127024
Pranjal Snehi <pranjal.snehi@gmail.com>
2025-08-18 21:08:24 +0530

    Added basic login screen

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,31 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+import "../../global.css";
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function AuthLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colorScheme === 'dark' ? '#1e293b' : '#fff' },
+        }}
+      />
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'light'} />
+    </ThemeProvider>
+  );
+}
